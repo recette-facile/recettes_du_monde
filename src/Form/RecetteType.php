af5b506839e2d52e98da649ea9cc4e0b2bd603e2
hiba <hbb.devpro@gmail.Com>
2021-07-20 15:18:34 +0200

    Exécution du crud pour Recette
    Création des fichiers : Controller/RecetteType et des templates

---
+++ src/Form/RecetteType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Recette;
+use App\Entity\ZoneGeo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecetteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nomRecette')
+            ->add('tempsPreparation')
+            ->add('tempsCuisson')
+            ->add('descriptionRecette')
+            ->add('zoneGeo', EntityType::class, [
+                'label' => 'cuisine typique de',
+                'placeholder' => '-- choisir une zone géographique  --',
+                'choice_label' => 'nom',
+                'class' => ZoneGeo::class
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Recette::class,
+        ]);
+    }
+}
